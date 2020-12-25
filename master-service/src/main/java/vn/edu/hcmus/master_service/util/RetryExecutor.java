75c6cd8a52388029bdba140c6b4133c2a41f89bf
Phu Nguyen <phu@treasure-data.com>
2020-12-25 16:37:07 +0700

    Add retry executor, update scheduledConfiguration

---
+++ master-service/src/main/java/vn/edu/hcmus/master_service/util/RetryExecutor.java
@@ -0,0 +1,102 @@
+package vn.edu.hcmus.master_service.util;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class RetryExecutor<T>
+{
+    private final Call<T> callable;
+    private int maxRetry = 3;
+    private int maxRetryWait = 120;
+
+    public RetryExecutor(final Call<T> callable)
+    {
+        this.callable = callable;
+    }
+
+    public Response<T> executeWithRetry()
+    {
+        Exception exception = null;
+        int currentRun = 0;
+        while (currentRun < maxRetry) {
+            try {
+                final Response<T> response = callable.clone().execute();
+                if (!response.isSuccessful()) {
+                    throw new RuntimeException(String.format("Error from server. Code : %s, Body: %s", response.code(), response.errorBody().string()));
+                }
+                return response;
+            }
+            catch (final Exception e) {
+                exception = e;
+            }
+            currentRun++;
+            long sleepTime = Math.round(Math.pow(2, currentRun));
+            sleepTime = sleepTime > maxRetryWait ? maxRetryWait : sleepTime;
+            try {
+                System.out.println(String.format("Sleeping %s to next retry", sleepTime));
+                Thread.sleep(sleepTime * 1000);
+            }
+            catch (final InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        throw new RuntimeException(exception);
+    }
+
+    public int getMaxRetry()
+    {
+        return maxRetry;
+    }
+
+    public void setMaxRetry(final int maxRetry)
+    {
+        this.maxRetry = maxRetry;
+    }
+
+    public int getMaxRetryWait()
+    {
+        return maxRetryWait;
+    }
+
+    public void setMaxRetryWait(final int maxRetryWait)
+    {
+        this.maxRetryWait = maxRetryWait;
+    }
+
+    public static class Builder<T>
+    {
+        private Call<T> callable;
+        private int maxRetry = 3;
+        private int maxRetryWait = 120;
+        public Builder() {}
+
+        public RetryExecutor<T> build()
+        {
+            if (callable == null) {
+                throw new IllegalArgumentException("Callable must be passed");
+            }
+            final RetryExecutor<T> executor = new RetryExecutor<>(callable);
+            executor.setMaxRetry(maxRetry);
+            executor.setMaxRetryWait(maxRetryWait);
+            return executor;
+        }
+
+        public Builder<T> withCallable(final Call<T> callable)
+        {
+            this.callable = callable;
+            return this;
+        }
+
+        public Builder<T> withMaxRetry(final int maxRetry)
+        {
+            this.maxRetry = maxRetry;
+            return this;
+        }
+
+        public Builder<T> withMaxRetryWait(final int maxRetryWait)
+        {
+            this.maxRetryWait = maxRetryWait;
+            return this;
+        }
+    }
+}
--- worker-service/src/main/java/vn/edu/hcmus/worker_service/ScheduledConfiguration.java
+++ worker-service/src/main/java/vn/edu/hcmus/worker_service/ScheduledConfiguration.java
@@ -1,39 +1,38 @@
 package vn.edu.hcmus.worker_service;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import vn.edu.hcmus.worker_service.api.MasterApi;
 import vn.edu.hcmus.worker_service.message.APIResponse;
 import vn.edu.hcmus.worker_service.message.WorkerStatusMessage;
 import vn.edu.hcmus.worker_service.util.AddressUtility;
+import vn.edu.hcmus.worker_service.util.RetryExecutor.Builder;
 import vn.edu.hcmus.worker_service.util.Utilities;
 
-import java.io.IOException;
-
-@Component
-public class StartUpComponent implements ApplicationListener<ApplicationReadyEvent>
+@Configuration
+@EnableScheduling
+public class ScheduledConfiguration
 {
-    @Override
-    public void onApplicationEvent(final ApplicationReadyEvent event)
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledConfiguration.class);
+
+    @Scheduled(initialDelay = 1_000, fixedDelay = 10_000)
+    public void updateStatus()
     {
         final WorkerStatusMessage message = new WorkerStatusMessage("worker", AddressUtility.getServerAddress(), AddressUtility.getServerPort(), 10, 0, 10);
         final Retrofit retrofit = Utilities.getRetrofit(String.format("http://%s:%s", AddressUtility.getMasterServerHost(), AddressUtility.getMasterServerPort()));
         final MasterApi api = retrofit.create(MasterApi.class);
         final Call<APIResponse<String>> request = api.updateStatus(message);
-        Response<APIResponse<String>> response;
         try {
-            response = request.execute();
-            System.out.println(response.isSuccessful());
-            System.out.println(response.code());
-            System.out.println(response.body());
-            System.out.println("PPPPPPPPPPPPPPPPPPPPPPP");
+            new Builder<APIResponse<String>>().withCallable(request).build().executeWithRetry();
         }
-        catch (final IOException e) {
-            e.printStackTrace();
+        catch (final Exception e) {
+            LOGGER.warn("Failed to update status");
+            LOGGER.warn(e.getMessage());
         }
     }
 }
