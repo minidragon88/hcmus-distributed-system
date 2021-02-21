b14aee00c8666bc2faf13f56bcc3d7729feaeb5c
Phu Nguyen <phu@treasure-data.com>
2021-02-21 16:58:23 +0700

    Update worker's service configuration

--- worker-service/src/main/java/vn/edu/hcmus/worker_service/ServiceConfiguration.java
+++ worker-service/src/main/java/vn/edu/hcmus/worker_service/ServiceConfiguration.java
@@ -2,29 +2,31 @@ package vn.edu.hcmus.worker_service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import vn.edu.hcmus.commons.api.MasterApi;
 import vn.edu.hcmus.commons.message.APIResponse;
 import vn.edu.hcmus.commons.message.WorkerStatusMessage;
 import vn.edu.hcmus.commons.utils.RetryExecutor;
 import vn.edu.hcmus.commons.utils.Utilities;
-import vn.edu.hcmus.worker_service.api.MasterApi;
-import vn.edu.hcmus.worker_service.util.AddressUtility;
+import vn.edu.hcmus.worker_service.util.EnvironmentUtility;
 
 @Configuration
 @EnableScheduling
-public class ScheduledConfiguration
+public class ServiceConfiguration
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledConfiguration.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceConfiguration.class);
 
     @Scheduled(initialDelay = 1_000, fixedDelay = 10_000)
     public void updateStatus()
     {
-        final WorkerStatusMessage message = new WorkerStatusMessage("worker", AddressUtility.getServerAddress(), AddressUtility.getServerPort(), 10, 0, 10);
-        final Retrofit retrofit = Utilities.getRetrofit(String.format("http://%s:%s", AddressUtility.getMasterServerHost(), AddressUtility.getMasterServerPort()));
+        final WorkerStatusMessage message = new WorkerStatusMessage("worker", EnvironmentUtility.getServerAddress(), EnvironmentUtility.getServerPort(), EnvironmentUtility.getCapacity(), 0, EnvironmentUtility.getCapacity());
+        final Retrofit retrofit = Utilities.getRetrofit(String.format("http://%s:%s", EnvironmentUtility.getMasterServerHost(), EnvironmentUtility.getMasterServerPort()));
         final MasterApi api = retrofit.create(MasterApi.class);
         final Call<APIResponse<String>> request = api.updateStatus(message);
         try {
@@ -34,5 +36,11 @@ public class ScheduledConfiguration
             LOGGER.warn("Failed to update status");
             LOGGER.warn(e.getMessage());
         }
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup()
+    {
+        System.out.println("hello world, I have just started up");
     }
 }
